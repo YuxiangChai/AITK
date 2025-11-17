fac755b0c5b4a0dd2fe8cdd36605ebe337981d81
Yuxiang Chai <seancyx@hotmail.com>
2025-11-17 15:51:04 +0800

    fix logger bug

--- aitk/__init__.py
+++ aitk/__init__.py
@@ -1,21 +1,8 @@
 __version__ = "0.2.1"
 
 
-import logging
 import platform
 from pathlib import Path
-
-aitk_logger = logging.getLogger("AITK - Controller")
-aitk_logger.setLevel(logging.INFO)  # Or DEBUG, WARNING, etc.
-
-# Prevent adding multiple handlers if re-imported
-if not aitk_logger.hasHandlers():
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
-    )
-    handler.setFormatter(formatter)
-    aitk_logger.addHandler(handler)
 
 
 def check_create_dir(path: Path) -> Path:
